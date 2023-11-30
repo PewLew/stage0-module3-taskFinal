54d750a9dc1845bb6ec3ed8b3a75df691615a7d2
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-11-30 18:07:52 +0600

    s

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double temperature = (((float)temperatureCelsius)*(9/5))+32;
+        double temperature = (((floats)temperatureCelsius)*(9/5))+32;
         System.out.println(temperature);
     }
 }
