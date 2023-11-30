7adfbdbf3056524b9af1a3c94d114b96b9e510d1
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-11-30 18:02:04 +0600

    s

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        int rem;
+
+        while (number > 0) {
+            rem = number % 10;
+            sum = sum + rem;
+            number = number / 10;
+        }
+        System.out.println(sum);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int rev = 0;
+        // remainder
+        int rem;
+
+        while (number > 0) {
+            rem = number % 10;
+            rev = (rev * 10) + rem;
+            number = number / 10;
+        }
+
+        System.out.println(rev);
     }
 }
